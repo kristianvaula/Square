94bf72ae0e012e6f1135a52f111ba4a650084652
Kristian Vaula <kristvje@stud.ntnu.no>
2023-03-20 10:51:08 +0100

    Created mock user log in store + bugfix

--- frontend/ecommerce-app/src/store/index.js
+++ frontend/ecommerce-app/src/store/index.js
@@ -1,13 +1,34 @@
 import { createStore } from 'vuex'
 
+/**
+ * Mocking a log in state for development
+ */
 export default createStore({
   state: {
+    user: {
+      loggedInStatus: false, 
+      username: ""
+    }
   },
   getters: {
+    getStatus(){
+      return this.user.loggedInStatus; 
+    },
+    getUsername() {
+      return this.user.username; 
+    }
   },
   mutations: {
   },
   actions: {
+    logIn(username) {
+      this.state.loggedInStatus = true; 
+      this.state.username = username; 
+    }, 
+    logOut() {
+      this.state.loggedInStatus = false; 
+      this.state.username = ""; 
+    }
   },
   modules: {
   }
