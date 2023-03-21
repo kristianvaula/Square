afc0ad99cc6249fa42122047d2bbbd437b105864
Kristian Vaula <kristvje@stud.ntnu.no>
2023-03-21 16:23:48 +0100

    Addded category, subcategory, prodimage models

---
+++ backend/ecommerce-app/src/main/java/ntnu/idatt2105/ecommerceapp/model/SubCategory.java
@@ -0,0 +1,39 @@
+package ntnu.idatt2105.ecommerceapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class SubCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column
+    private String description;
+    @Column
+    private int categoryId;
+
+    public SubCategory() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+}
