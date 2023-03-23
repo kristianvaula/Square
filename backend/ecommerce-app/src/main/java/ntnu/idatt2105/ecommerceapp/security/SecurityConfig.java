e455799e67ef04ec5d3bffefb1fbba9031369c36
Espen Justad <espenjus@stud.ntnu.no>
2023-03-23 13:39:19 +0100

    added method to remove security

--- backend/ecommerce-app/src/main/java/ntnu/idatt2105/ecommerceapp/security/SecurityConfig.java
+++ backend/ecommerce-app/src/main/java/ntnu/idatt2105/ecommerceapp/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -27,6 +28,12 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    /**@Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().anyRequest();
+    }
+    **/
 
     //todo: slette?
     @Bean
