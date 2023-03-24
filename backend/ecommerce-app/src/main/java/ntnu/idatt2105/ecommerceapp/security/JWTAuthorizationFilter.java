c0263213ee537bbc97ea1195c0aa274810a86be7
Hans Magne Asheim <hmasheim@stud.ntnu.no>
2023-03-24 14:29:44 +0100

    edit: Filter file

--- backend/ecommerce-app/src/main/java/ntnu/idatt2105/ecommerceapp/security/JWTAuthorizationFilter.java
+++ backend/ecommerce-app/src/main/java/ntnu/idatt2105/ecommerceapp/security/JWTAuthorizationFilter.java
@@ -11,8 +11,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import ntnu.idatt2105.ecommerceapp.services.TokenService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -26,43 +29,49 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
     private static final Logger LOGGER = LogManager.getLogger(JWTAuthorizationFilter.class);
     public static final String USER = "USER";
     public static final String ROLE_USER = "ROLE_" + USER;
+    @Autowired
+    private CustomAuthenticationProvider authProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        // check Bearer auth header
         final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String jwtToken;
+        final String eMail;
+
         if (header == null || !header.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        // if Bearer auth header exists, validate token, and extract userId from token.
+        // if Bearer auth header exists, validate token, and extract userEmail from token.
         // Note that we have added userId as subject to the token when it is generated
         // Note also that the token comes in this format 'Bearer token'
-        String token = header.substring(7);
-        final String username = validateTokenAndGetUserId(token);
-        if (username == null) {
+        jwtToken = header.substring(7);
+        eMail = validateTokenAndGetUserEmail(jwtToken);
+        if (eMail == null) {
             // validation failed or token expired
             filterChain.doFilter(request, response);
             return;
         }
 
+
         // if token is valid, add user details to the authentication context
         // Note that user details should be fetched from the database in real scenarios
         // this is case we will retrieve use details from mock
         UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
-                username,
+                eMail,
                 null,
                 Collections.singletonList(new SimpleGrantedAuthority(ROLE_USER)));
         SecurityContextHolder.getContext().setAuthentication(auth);
 
         // then, continue with authenticated user context
+
         filterChain.doFilter(request, response);
     }
 
-    public String validateTokenAndGetUserId(final String token) {
+    public String validateTokenAndGetUserEmail(final String token) {
         try {
-            final Algorithm hmac512 = Algorithm.HMAC512(TokenService.KEY);;
+            final Algorithm hmac512 = Algorithm.HMAC512(TokenService.KEY);
             final JWTVerifier verifier = JWT.require(hmac512).build();
             return verifier.verify(token).getSubject();
         } catch (final JWTVerificationException verificationEx) {
