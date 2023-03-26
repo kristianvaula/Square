105001f834d8348a3501e4efabd288439472fb9c
Kristian Vaula <kristvje@stud.ntnu.no>
2023-03-26 16:59:13 +0200

    Utils calls support authorization

--- frontend/ecommerce-app/src/utils/ProductUtils.js
+++ frontend/ecommerce-app/src/utils/ProductUtils.js
@@ -1,21 +1,73 @@
 import axios from 'axios';
+import { useTokenStore } from '@/store/token'; 
 
 const baseurl = "http://localhost:8081";
 
+const defaultConfig = {
+  headers: {
+    "Content-type" : "application/json"
+  }
+}
+
+function getUserConfig(){
+  let tokenStore = useTokenStore();
+  console.log(tokenStore.jwtToken) 
+  return {
+    headers: {
+      "Content-type": "application/json",
+      "Authorization": "Bearer " + tokenStore.jwtToken
+  },
+  }
+}
+
+const getMimeType = fileName => {
+  const extension = fileName.split(".").pop();
+  switch (extension) {
+    case "jpg":
+    case "jpeg":
+      return "image/jpeg";
+    case "png":
+      return "image/png";
+    case "gif":
+      return "image/gif";
+    default:
+      return "";
+  }
+};
+
+const getImageSrc = image => {
+  const mimeType = getMimeType(image.name);
+  return `data:${mimeType};base64,${image.image}`;
+};
+
+const getProductsWithImages = response => response.data.map(productResponse => ({
+  product: productResponse.product,
+  imageList: productResponse.imageList.map(image => ({
+    name: image.name,
+    src: getImageSrc(image)
+  }))
+}));
+
 export default {
+  
   createProduct(formData) {
-    return axios.post(baseurl + "/product/new", formData); 
+    return axios.post(baseurl + "/unauthorized/product/new", formData, getUserConfig()); 
   }, 
   getProducts() {
-    return axios.get(baseurl + "/product/all"); 
-  }, 
+    let config = getUserConfig()
+    return axios.get(baseurl + "/unauthorized/product/all", config)
+    .then(getProductsWithImages)
+  },
   getProductById(id) {
-    return axios.get(baseurl + `/product/${id}`);
+    return axios.get(baseurl + `/unauthorized/product/${id}`, defaultConfig)
+    .then(getProductsWithImages)
   },
   getProductByCategory(category) {
-    return axios.get(baseurl + `/product/category/${category}`);
+    return axios.get(baseurl + `/unauthorized/product/category/${category}`, defaultConfig)
+    .then(getProductsWithImages)
   },
   getProductBySubcategory(subcategory) {
-    return axios.get(baseurl + `/product/subcategory/${subcategory}`);
+    return axios.get(baseurl + `/unauthorized/product/subcategory/${subcategory}`, defaultConfig)
+    .then(getProductsWithImages)
   },
 }
