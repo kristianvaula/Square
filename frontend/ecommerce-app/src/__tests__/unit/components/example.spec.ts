8e9333e3bec5b3f85825646c00859458e29e9472
Kristian Vaula <kristvje@stud.ntnu.no>
2023-03-27 11:16:36 +0200

    Test fixes

---
+++ frontend/ecommerce-app/src/__tests__/unit/components/example.spec.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect, beforeEach, afterEach} from 'vitest'; 
+import { shallowMount } from "@vue/test-utils";
+import CategoryCard from "@/components/CategoryCard.vue";
+
+describe("CategoryCard tests", () => {
+  let wrapper;
+  const categoryInfo = {
+    categoryId: 1,
+    description: "Category 1",
+    size: 2,
+  };
+
+  beforeEach(() => {
+    wrapper = shallowMount(CategoryCard, {
+      propsData: {
+        CategoryInfo: categoryInfo,
+      },
+    });
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it("renders the correct category title", () => {
+    const title = wrapper.find(".category-title h3");
+    expect(title.text()).toEqual(categoryInfo.description);
+  });
+
+  it("renders the correct category size", () => {
+    const size = wrapper.find(".category-size span");
+    expect(size.exists()).toBe(true);
+    expect(size.text()).toEqual(`(${categoryInfo.size})`);
+    wrapper.vm.$forceUpdate();
+  });
+
+  it("emits 'selectedCardEvent' event when clicked and category is not selected", () => {
+    wrapper.trigger("click");
+    expect(wrapper.emitted("selectedCardEvent")).toBeTruthy();
+    expect(wrapper.emitted("selectedCardEvent")[0][0]).toEqual(categoryInfo);
+  });
+
+  it("emits 'deselectedCardEvent' event when clicked and category is already selected", () => {
+    wrapper.vm.store.CurrentCategoryID = categoryInfo.categoryId;
+    wrapper.trigger("click");
+    expect(wrapper.emitted("deselectedCardEvent")).toBeTruthy();
+    expect(wrapper.emitted("deselectedCardEvent")[0][0]).toEqual(categoryInfo);
+  });
+});
--- frontend/ecommerce-app/src/__tests__/unit/router/example.spec.ts
+++ frontend/ecommerce-app/src/__tests__/unit/router/example.spec.ts
@@ -1,3 +1,5 @@
+import { describe, it, expect} from 'vitest'; 
+
 describe('Possible to run tests', () => {
   it('first test', () => {
     expect(true).toBe(true)
